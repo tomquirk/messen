daa432dd1b299a9f0541a51218199283e40bd5c3
Tom Quirk <tom@coviu.com>
2018-11-17 11:35:57 +1000

    prompt for mfa code in tests

--- test/messy.test.ts
+++ test/messy.test.ts
@@ -1,14 +1,30 @@
 import { expect } from 'chai';
-import Messy from '../src/messy';
+import readline from 'readline';
 
+import Messy from '../src/messy';
 import config from '../config/test.json';
+
+function promptCode(): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise(resolve => {
+    console.log('Enter code > ');
+    return rl.on('line', line => {
+      resolve(line);
+      rl.close();
+    });
+  });
+}
 
 describe('Messy', () => {
   let messy: Messy;
   beforeEach(() => {
     messy = new Messy();
     messy.getMfaCode = () => {
-      return Promise.resolve('967485');
+      return promptCode();
     };
   });
 
