4dd31bfd30f8a2c5e1edbee5f2440e85415bc249
Tom Quirk <tomquirkacc@gmail.com>
2019-04-20 06:53:08 +1000

    finish tests for now

--- test/unit/store/threads.test.ts
+++ test/unit/store/threads.test.ts
@@ -10,28 +10,28 @@ describe('Thread Store', function () {
   });
 
   it('should be able to get a thread by id', async function () {
-    threadStore.getThread({ id: '100003961877411' }).then(thread => {
+    await threadStore.getThread({ id: '100003961877411' }).then(thread => {
       expect(thread).to.exist;
       expect(thread.threadID).to.equal('100003961877411')
     })
   });
 
   it('should be able to get a thread by name', async function () {
-    threadStore.getThread({ name: 'tom quirk' }).then(thread => {
+    await threadStore.getThread({ name: 'tom quirk' }).then(thread => {
       expect(thread).to.exist;
       expect(thread.threadID).to.equal('100003961877411')
     })
   });
 
   it('should be able to get a thread by fuzzy thread name', async function () {
-    threadStore.getThread({ name: 'to' }).then(thread => {
+    await threadStore.getThread({ name: 'to' }).then(thread => {
       expect(thread).to.exist;
       expect(thread.threadID).to.equal('100003961877411')
     })
   });
 
   it('should be able to get a thread by id when query contains both name and id', async function () {
-    threadStore.getThread({ id: '100003961877411', name: 'ahhaha' }).catch(thread => {
+    await threadStore.getThread({ id: '100003961877411', name: 'ahhaha' }).catch(thread => {
       expect(thread).to.exist;
       expect(thread.threadID).to.equal('100003961877411')
     })
--- test/unit/store/users.test.ts
+++ test/unit/store/users.test.ts
@@ -10,30 +10,37 @@ describe('User Store', function () {
   });
 
   it('should be able to get a user by id', async function () {
-    userStore.getUser({ id: '100003961877411' }).then(user => {
+    await userStore.getUser({ id: '100003961877411' }).then(user => {
       expect(user).to.exist;
       expect(user.id).to.equal('100003961877411')
     })
   });
 
   it('should be able to get a user by name', async function () {
-    userStore.getUser({ name: 'tom quirk' }).then(user => {
+    await userStore.getUser({ name: 'tom quirk' }).then(user => {
       expect(user).to.exist;
       expect(user.id).to.equal('100003961877411')
     })
   });
 
   it('should be able to get a user by fuzzy user name', async function () {
-    userStore.getUser({ name: 'to' }).then(user => {
+    await userStore.getUser({ name: 'to' }).then(user => {
       expect(user).to.exist;
       expect(user.id).to.equal('100003961877411')
     })
   });
 
   it('should be able to get a user by id when query contains both name and id', async function () {
-    userStore.getUser({ id: '100003961877411', name: 'ahhaha' }).catch(user => {
+    await userStore.getUser({ id: '100003961877411', name: 'ahhaha' }).catch(user => {
       expect(user).to.exist;
       expect(user.id).to.equal('100003961877411')
     })
+  });
+
+  it('should be able to get the currently logged in user (me user)', function () {
+    expect(userStore.me.user).to.exist
+    expect(userStore.me.user.id).to.equal('100035969370185')
+    expect(userStore.me.friends.length).to.be.above(0)
+    expect(userStore.me.friends[0].id).to.equal('100003961877411')
   });
 });
