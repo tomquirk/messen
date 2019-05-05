337b2e220c0ea0d1c65bd3b24231c8c662b44b3d
Tom Quirk <tomquirkacc@gmail.com>
2019-05-06 07:19:15 +1000

    require -> import

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,4 +1,4 @@
-const { createLogger, format, transports } = require('winston');
+import { createLogger, format, transports } from 'winston';
 import { ENVIRONMENT } from '../settings';
 
 const getlogger = (name: string) =>
